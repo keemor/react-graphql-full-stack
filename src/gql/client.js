2b2d14f3945a435d1f81f6cad76e8152e8c24e38
keemor <keemor@keemor.com>
2019-04-29 12:37:32 +0200

    Cient cleanup

--- src/gql/client.js
+++ src/gql/client.js
@@ -7,17 +7,15 @@ import ApolloClient from 'apollo-boost';
 const client = new ApolloClient({
     uri: '/graphql',
     onError: ({ graphQLErrors, networkError }) => {
-        //console.log('graphQLErrors, networkError: ', graphQLErrors, networkError);
         if (graphQLErrors.length) {
             render(
                 <UncontrolledAlert color="danger">{graphQLErrors[0].message}</UncontrolledAlert>,
                 document.getElementById('error')
             );
         }
-        //if (networkError) console.log(`xxxxxxxxxx   [Network error]: ${networkError}`);
     },
     request: async operation => {
-        // const token = await localStorage.getItem('token');
+        render(<span />, document.getElementById('error'));
         const token = await Cookies.get('token');
         operation.setContext({
             headers: {
